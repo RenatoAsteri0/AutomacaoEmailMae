335a052b03be59fbc21cc95396f8837710d3282c
Renato De Lima Asterio <renato.asterio@ultrapar.corp>
2025-09-08 17:31:54 -0300

    add

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import pandas as pd
 # 1
 df = pd.read_excel('Planilha Renato.xlsx', sheet_name='Prospects Insac', usecols='A:E')
 
-df = df.map(lambda x: x.strip().replace('\n', '') if isinstance(x, str) else x)
+df = df.map(lambda x: x.strip().replace('\n', '').replace('-', '').replace('(', '').
+            replace(')', '') if isinstance(x, str) else x)
 capitalizar_colums = ['Cliente prospect', 'Contato', 'Cidade']
 df[capitalizar_colums] = df[capitalizar_colums].apply(lambda x: x.str.title())
+to_excel = df.to_excel('ClienteMarciaArrumaTelefones.xlsx', index=False)
 emails = df[['Cliente prospect', 'Contato', 'e-mail', 'telefone', 'Cidade']].to_dict('records')
 print(emails)
